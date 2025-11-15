1c8d3cbbe416c9d9d56ad9c8d29ed574e73b3cdd
ArsalJan34 <arsaljaan532@gmail.com>
2025-11-15 13:48:48 +0500

    Changes made

---
+++ circumference.js
@@ -0,0 +1,6 @@
+function calcCircumference(radius) {
+    let circumference = 2 * 3.14 * radius;
+    console.log("The circumference is " + circumference);
+}
+
+calcCircumference(5);
---
+++ radiusArea.js
@@ -0,0 +1,6 @@
+function calcArea(radius) {
+    let area = 3.14 * radius * radius;
+    console.log("The area is " + area);
+}
+
+calcArea(5);
