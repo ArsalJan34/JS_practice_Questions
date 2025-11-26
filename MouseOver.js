f6d83363e3944dc32590e42c7ddb0f3d2cd26c48
ArsalJan34 <arsaljaan532@gmail.com>
2025-11-26 19:13:35 +0500

    initial commit

--- MouseOver.js
+++ MouseOver.js
@@ -1,11 +1,8 @@
     document.getElementById("demo").onmouseover = function () {
       this.style.color = "red";
-    };
+    }
 
-
-
-    
-
+  
     document.getElementById("demo").onmouseout = function () {
       this.style.color = "black"; // Turn back to black when mouse leaves
     };
